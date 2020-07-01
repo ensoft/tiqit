12070227a37c2faffa600568c1c747c4a3ef07a5
joloh <joloh@cisco.com>
2020-07-01 18:14:40 +0100

    Add fix for the case where the plugin does not provide any cookies.

--- scripts/tiqit/__init__.py
+++ scripts/tiqit/__init__.py
@@ -45,7 +45,7 @@ times = [("start", time.time())]
 
 MAJ_VER   = 1
 MIN_VER   = 0
-PATCH_VER = 8
+PATCH_VER = 9
 DEV_VER   = 0
 
 VERSION = (MAJ_VER, MIN_VER, PATCH_VER)
@@ -1120,9 +1120,11 @@ def printPageHeader(pageName, pageTitle="", initScript=None, otherHeaders=[],
     bodyClass = bugView and bugView.bodyClass or ''
     
     # Print the HTTP header, including any cookies.
-    for c in plugins.getOutgoingCookies():
-        print c
-    print "Set-Cookie: update=; expires=Sat, 01-Jan-2000 00:00:00 GMT; path=%s" % getBasePath()
+    outgoingCookies = plugins.getOutgoingCookies()
+    if outgoingCookies:
+        for c in outgoingCookies:
+            print c
+        print "Set-Cookie: update=; expires=Sat, 01-Jan-2000 00:00:00 GMT; path=%s" % getBasePath()
     print "Content-Type: text/html; charset=utf-8"
     print """
 <html>
