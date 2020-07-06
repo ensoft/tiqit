f33aa2f91dc8bb7f6c14e20a9364af2c60e7d962
joloh <joloh@cisco.com>
2020-07-06 17:25:57 +0100

    Remove the contents of the "update" cookies between pages.

--- scripts/tiqit/__init__.py
+++ scripts/tiqit/__init__.py
@@ -1124,7 +1124,7 @@ def printPageHeader(pageName, pageTitle="", initScript=None, otherHeaders=[],
     if outgoingCookies:
         for c in outgoingCookies:
             print c
-        print "Set-Cookie: update=; expires=Sat, 01-Jan-2000 00:00:00 GMT; path=%s" % getBasePath()
+    print "Set-Cookie: update=; Max-Age=0; path=%s" % getBasePath()
     print "Content-Type: text/html; charset=utf-8"
     print """
 <html>
