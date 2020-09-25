4bc3278a4965ff26677ba2fd2762f9aefc6f8af9
joloh <joloh@cisco.com>
2020-09-25 15:19:35 +0100

    Hotfix/multiedit field editing for dev/ branch (#41)
    Multiedit fix.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from distutils.core import setup
 
 setup(name='tiqit',
-      version="1.0.10",
+      version="1.0.11",
       description='Tiqit: The Intelligent Issue Tracker',
       url='https://launchpad.net/tiqit',
       maintainer='Tiqit maintainers at Ensoft',
