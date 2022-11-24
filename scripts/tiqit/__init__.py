947726a7c3e810f2e621fb74762d733ce2d1c06f
Olli Johnson <olijohns@cisco.com>
2022-11-24 11:43:31 +0000

    Fix bad var name

--- scripts/tiqit/__init__.py
+++ scripts/tiqit/__init__.py
@@ -1141,8 +1141,8 @@ def printPageHeader(pageName, pageTitle="", initScript=None, otherHeaders=[],
         # The plugin may provide a version along with the script
         # in which case it is added to the tiqit version
         pluginVersion = ""
-        if isinstance(script, tuple):
-            script, plugin_version = script
+        if isinstance(style, tuple):
+            style, plugin_version = style
         print("<link rel='stylesheet' type='text/css' href='styles/%s.css?version=%s' media='screen'>" % (
             style,
             VERSION_STRING,
