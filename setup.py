0b044cfb7707865f7f7be61c5c9863451c140b1d
joloh <joloh@cisco.com>
2024-09-27 14:41:41 +0100

    Feature/updatebug project (#64)
    * Add project data to what is passed into backend
    * Update version info

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from distutils.core import setup
 
 setup(name='tiqit',
-      version="1.1.2",
+      version="1.1.3",
       description='Tiqit: The Intelligent Issue Tracker',
       url='https://github.com/ensoft/tiqit',
       maintainer='Tiqit maintainers at Ensoft',
